42f8e1f075e3977dabbc8ec26f99860c73212a06
Gabriel Wicke <gwicke@wikimedia.org>
2014-01-14 15:07:00 -0800

    Remove unnecessary wikis from localsettings
    * tyv and min are now added to our list
    
    * hyphen to underscore replacement in aliases is now done automatically in
      ParsoidConfig
    
    Change-Id: Ie8d665e97361a2f197942642bfbe65a1b5417cab

--- conf/wmf/localsettings.js
+++ conf/wmf/localsettings.js
@@ -20,26 +20,6 @@ exports.setup = function( parsoidConfig ) {
                 parsoidConfig.setInterwiki( dbname, interwikiMap[dbname], null );
         }
 
-        // tyv and min wikis are missing from the list of known wikipedias
-        // in mediawiki.ParsoidConfig.js.  So, adding it here explicitly.
-        parsoidConfig.setInterwiki('tyvwiki', 'http://tyv.wikipedia.org/w/api.php');
-        parsoidConfig.setInterwiki('minwiki', 'http://min.wikipedia.org/w/api.php');
-
-        // The following wikis with a hyphen in wikiname aren't probably handled
-        // by Parsoid: "-" should be converted to "_" in ParsoidConfig. But, for today,
-        // we'll add the following wikis to get VE unbroken on these.
-        parsoidConfig.setInterwiki('bat_smgwiki', 'http://bat-smg.wikipedia.org/w/api.php');
-        parsoidConfig.setInterwiki('be_x_oldwiki', 'http://be-x-old.wikipedia.org/w/api.php');
-        parsoidConfig.setInterwiki('cbk_zamwiki', 'http://cbk-zam.wikipedia.org/w/api.php');
-        parsoidConfig.setInterwiki('fiu_vrowiki', 'http://fiu-vro.wikipedia.org/w/api.php');
-        parsoidConfig.setInterwiki('map_bmswiki', 'http://map-bms.wikipedia.org/w/api.php');
-        parsoidConfig.setInterwiki('nds_nlwiki', 'http://nds-nl.wikipedia.org/w/api.php');
-        parsoidConfig.setInterwiki('roa_rupwiki', 'http://roa-rup.wikipedia.org/w/api.php');
-        parsoidConfig.setInterwiki('roa_tarawiki', 'http://roa-tara.wikipedia.org/w/api.php');
-        parsoidConfig.setInterwiki('zh_classicalwiki', 'http://zh-classical.wikipedia.org/w/api.php');
-        parsoidConfig.setInterwiki('zh_min_nanwiki', 'http://zh-min-nan.wikipedia.org/w/api.php');
-        parsoidConfig.setInterwiki('zh_yuewiki', 'http://zh-yue.wikipedia.org/w/api.php');
-
         parsoidConfig.usePHPPreProcessor = true;
         parsoidConfig.useSelser = true;
         // parsoid cache url
