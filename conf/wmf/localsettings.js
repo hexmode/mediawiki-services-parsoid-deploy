05993bf91bd466b0a9a092d993aaca6c6a2dfd04
James D. Forrester <jforrester@wikimedia.org>
2014-01-17 14:36:32 -0800

    Clean up interwiki map
    Also, split the ones that actually need to bypass the proxy from the others.
    
    Change-Id: I9c192c78b1b7b64c8429810b926184713690be52

--- conf/wmf/localsettings.js
+++ conf/wmf/localsettings.js
@@ -1,23 +1,29 @@
 exports.setup = function( parsoidConfig ) {
         // Map from DB name to API URL
-        var dbname, interwikiMap = {
-                'testwiki': 'http://test.wikipedia.org/w/api.php',
-                'test2wiki': 'http://test2.wikipedia.org/w/api.php',
+        var dbname,
+                interwikiMap = {
+                        'testwiki': 'http://test.wikipedia.org/w/api.php',
+                        'test2wiki': 'http://test2.wikipedia.org/w/api.php',
 
-                'sewikimedia': 'http://se.wikimedia.org/w/api.php',
+                        'sewikimedia': 'http://se.wikimedia.org/w/api.php' },
 
-                'boardwiki': 'https://board.wikimedia.org/w/api.php',
-                'collabwiki': 'https://collab.wikimedia.org/w/api.php',
-                'officewiki': 'https://office.wikimedia.org/w/api.php',
-                'wikimaniateamwiki': 'https://wikimaniateam.wikimedia.org/w/api.php',
+                privateInterwikiMap = {
+                        'boardwiki': 'https://board.wikimedia.org/w/api.php',
+                        'collabwiki': 'https://collab.wikimedia.org/w/api.php',
+                        'officewiki': 'https://office.wikimedia.org/w/api.php',
+                        'wikimaniateamwiki': 'https://wikimaniateam.wikimedia.org/w/api.php',
 
-                'labswiki': 'https://wikitech.wikimedia.org/w/api.php'
+                        'labswiki': 'https://wikitech.wikimedia.org/w/api.php' // Not private but can't use proxy
         };
 
         for ( dbname in interwikiMap ) {
+                parsoidConfig.setInterwiki( dbname, interwikiMap[dbname] );
+        }
+
+        for ( dbname in privateInterwikiMap ) {
                 // Disable proxying for special wikis
                 // TODO: See if we can go direct for some of those too
-                parsoidConfig.setInterwiki( dbname, interwikiMap[dbname], null );
+                parsoidConfig.setInterwiki( dbname, privateInterwikiMap[dbname], null );
         }
 
         parsoidConfig.usePHPPreProcessor = true;
